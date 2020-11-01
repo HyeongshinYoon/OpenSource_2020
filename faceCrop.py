70ee200eeb5ad9a59c23f58d80ff64bce945df8e
HyeongshinYoon <tkxkd7223@gmail.com>
2020-11-01 22:28:04 +0900

    Update faceCrop.py

--- faceCrop.py
+++ faceCrop.py
@@ -1,8 +1,5 @@
-import cv2
-import os
 from openpyxl import Workbook, load_workbook
-
-# 해야할 일: face morph
+import cv2
 
 
 def image_crop_process(n):
@@ -40,50 +37,46 @@ def image_crop_process(n):
         write_ws.append(
             [i+1, './resize_image{}.jpg'.format(i+1), rx, ry, rw, rh, width, height])
     write_wb.save('./image_crop.xlsx')
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 
-def image_merge_process():
+def image_merge_process(body, face):
     load_wb = load_workbook("./image_crop.xlsx", data_only=True)
     load_ws = load_wb['Sheet']
-    idx = 1
-    for row in load_ws.rows:
-        if idx == 1:
-            idx += 1
-            continue
-        idx += 1
-        row_value = []
-        for cell in row:
-            row_value.append(cell.value)
-        [num, address, rx, ry, rw, rh, w, h] = row_value
-        gan_address = "./crop_image" + str(1) + ".jpg"
-        img = cv2.imread(address)
-        y1, y2 = int(ry - int(rh/8)), int(ry + rh + int(rh/8))
-        x1, x2 = int(rx - int(rw/8)), int(rx + rw + int(rw/8))
-        gan_img = cv2.resize(cv2.imread(gan_address),
-                             (y2-y1, x2-x1), interpolation=cv2.INTER_CUBIC)
+    row = load_ws[body+1]
+    row_value = []
+    for cell in row:
+        row_value.append(cell.value)
+    [num, address, rx, ry, rw, rh, w, h] = row_value
+    print(rx)
+    gan_address = "./crop_image" + str(face) + ".jpg"
+    img = cv2.imread(address)
+    y1, y2 = int(ry - int(rh/8)), int(ry + rh + int(rh/8))
+    x1, x2 = int(rx - int(rw/8)), int(rx + rw + int(rw/8))
+    gan_img = cv2.resize(cv2.imread(gan_address),
+                         (y2-y1, x2-x1), interpolation=cv2.INTER_CUBIC)
 
-# If you want to use only changed image using GAN, you use next line only.
-#        img[y1:y2, x1:x2] = gan_img
+    # If you want to use only changed image using GAN, you use next line only.
+    #        img[y1:y2, x1:x2] = gan_img
 
-# Mixing background and GAN image.
-        alpha_s = 0.9
-        alpha_l = 1.0 - alpha_s
+    # Mixing background and GAN image.
+    alpha_s = 0.5
+    alpha_l = 1.0 - alpha_s
+    for color in range(0, 3):
+        img[y1:y2, x1:x2, color] = (alpha_s * gan_img[:, :, color] +
+                                    alpha_l * img[y1:y2, x1:x2, color])
 
-        for color in range(0, 3):
-            img[y1:y2, x1:x2, color] = (alpha_s * gan_img[:, :, color] +
-                                        alpha_l * img[y1:y2, x1:x2, color])
-
-# Save mixing image.
-        resize_img = cv2.resize(
-            img, (w, h), interpolation=cv2.INTER_CUBIC + cv2.INTER_LINEAR)
-        cv2.imwrite("merge_image{}.jpg".format(num), resize_img)
+    # Save mixing image.
+    resize_img = cv2.resize(
+        img, (w, h), interpolation=cv2.INTER_CUBIC + cv2.INTER_LINEAR)
+    cv2.imwrite("merge_image{}.jpg".format(num), resize_img)
 
 
 def main():
     image_crop_process(5)
-    image_merge_process()
+
+    body = int(input())
+    face = int(input())
+    image_merge_process(body, face)
 
 
 if __name__ == "__main__":
