e25332c6400648a8031621500105e2cd4d86bef8
totolacky <totolacky@kaist.ac.kr>
2020-11-07 16:13:53 +0900

    Add StyleGAN

---
+++ face_gen/mean_style.py
@@ -0,0 +1,17 @@
+import torch
+
+@torch.no_grad()
+def get_mean_style(generator, device):
+    mean_style = None
+
+    for i in range(10):
+        style = generator.mean_style(torch.randn(1024, 512).to(device))
+
+        if mean_style is None:
+            mean_style = style
+
+        else:
+            mean_style += style
+
+    mean_style /= 10
+    return mean_style
