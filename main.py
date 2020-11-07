8a00a9fbfd16f0d28146d5fcfb04341fe29e63c9
HyeongshinYoon <tkxkd7223@gmail.com>
2020-11-08 02:58:07 +0900

    merge face_gen

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ from face_swap.face_swap import face_swap
 from face_gen.generate import face_gen
 from face_gen.mix import style_mixing
 
+
+def refresh_image(n):
+    style_vector = []
+    for i in range(n):
+        style = face_gen("new_face"+str(i)).tolist()
+        style_vector.append(style)
+
+    return style_vector
+
+
+def mixing_image(style, n):
+
+    style_vector = refresh_image(n)
+
+    for i in range(n):
+        style_mixing(style, style_vector[i], "new_face"+str(i+n))
+
+
 def main():
 
     #body_input = int(input())
@@ -12,16 +30,23 @@ def main():
     body = cv2.imread(body_address)
     body_x, body_y, body_w, body_h, old_face, resized_body = image_divide(body)
 
-    #new_face_input = int(input())
-    # get image from styleGAN
-#    new_face_input = styleGAN()
-    new_face_input = "./crop_image5.jpg"
+    # make and choose new faces
+    new_style = refresh_image(5)
+    mixing_image(new_style[0], 5)
+
+    # selecting face
+    new_face_input = "./new_face4.jpg"
     new_face = cv2.imread(new_face_input)
+
+    # swap origin face to selecting face
     swap_face = face_swap(old_face, new_face)
     cv2.imwrite("swap.jpg", swap_face)
+
+    # merge to body and changed face
     swap_img = image_merge_process(resized_body, swap_face,
                                    body_x, body_y, body_w, body_h)
-    #result_img = viton(swap_img)
+
+    # result_img = viton(swap_img)
     cv2.imwrite("result_img.jpg", swap_img)
 
 
